698071d9a9b16db1f6ec82c113e7e4b068c9e225
j-thrash <j-thrash@ya.ru>
2023-01-24 15:52:42 +0300

    Initial commit

---
+++ src/main/java/com/example/aston_03_jdbc/Aston03JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.aston_03_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aston03JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aston03JdbcApplication.class, args);
+    }
+
+}
